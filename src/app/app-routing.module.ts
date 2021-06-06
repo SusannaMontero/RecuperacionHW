d42d901ed5da23f3ab84f66f4454745af3421374
LxEvan <cristianctl1@gmail.com>
2021-06-06 19:11:52 +0200

    Commit Ingles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { EditarPerfilComponent } from './components/esp/esperfilus/editar-perfil/editar-perfil.component';
+import { EditProfileComponent } from './components/eng/inperfilus/edit-profile/edit-profile.component';
 import { EsperfilusComponent } from './components/esp/esperfilus/esperfilus.component';
+import { InperfilusComponent } from './components/eng/inperfilus/inperfilus.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './components/esp/index/index.component';
@@ -12,7 +14,7 @@ import { InindexComponent } from './components/eng/inindex/inindex.component';
 import { InheaderComponent } from './components/eng/inheader/inheader.component';
 import { AbouthealthworldComponent } from './components/eng/abouthealthworld/abouthealthworld.component';
 import { ContactComponent } from './components/eng/contact/contact.component';
-import { InloginComponent } from './components/eng/inlogin/inlogin.component';
+
 
 
 const routes: Routes = [{path: '', pathMatch: 'full', redirectTo: 'index'},
@@ -26,9 +28,10 @@ const routes: Routes = [{path: '', pathMatch: 'full', redirectTo: 'index'},
 { path: 'inheader', component: InheaderComponent},
 { path: 'abouthealthworld', component: AbouthealthworldComponent},
 { path: 'contact', component: ContactComponent},
-{ path: 'inlogin', component: InloginComponent},
 { path: 'esperfilus', component: EsperfilusComponent},
+{ path: 'inperfilus', component: InperfilusComponent},
 { path: 'editarPerfil', component: EditarPerfilComponent},
+{ path: 'editProfile', component: EditProfileComponent},
 { path: '**', redirectTo: 'index'}
 
 ];
